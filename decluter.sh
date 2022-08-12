c69b4b5c1b0b39d99bf46649886c5bb5cebd048e
Mykell404 <milebamicheal@gmail.com>
2022-08-12 12:43:02 +0100

    updated decluter

--- decluter.sh
+++ decluter.sh
@@ -3,4 +3,21 @@
 echo "Name of the folder"
 read FOLDER 
 
+cd ..
+cd Desktop
 mkdir ${FOLDER}
+
+desktop_folders=$(ls)
+for FOLDERS in $desktop_folders
+do
+  # echo ${FOLDERS}
+  if [ "$FOLDER" = "$FOLDERS" ] 
+  then
+    echo "Can't copy this"
+    # echo ${FOLDER}
+  else
+    mv "${FOLDERS}" "${FOLDER}/${FOLDERS}"
+    # echo ${FOLDER}
+  fi
+done
+
